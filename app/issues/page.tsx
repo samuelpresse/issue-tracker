2926f24995773ada9c7fecce6c4b4db0a7448ad6
samuelpresse <samuelpresse@gmail.com>
2024-12-07 20:04:53 -0500

    Add markdown preview

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,14 +1,12 @@
 import React from "react";
-import { Button, Table } from "@radix-ui/themes";
+import { Table } from "@radix-ui/themes";
 import Link from "next/link";
 import prisma from "@/prisma/client";
 import IssueStatusBadge from "../components/IssueStatusBadge";
-import delay from "delay";
 import IssueActions from "./IssueActions";
 
 const IssuesPage = async () => {
   const issues = await prisma.issue.findMany({});
-  await delay(2000);
   return (
     <div>
       <IssueActions />
